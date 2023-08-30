68f113e43e41eae326a1d9d7f2fcaadab4edda11
Steven Kitterman <steven.kitterman@gmail.com>
2023-08-30 12:37:38 -0700

    Some filez

---
+++ tests/example.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+const { test, expect } = require('@chromaui/test-archiver')
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByRole('link', { name: 'Learn React' })).toBeVisible();
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
